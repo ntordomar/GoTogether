f5d00d5056fa8d24856fc666f1076af71601b95f
felipemindlin <fmindlin@itba.edu.ar>
2023-04-01 13:46:03 -0300

    Se agrego pantalla de User e intento de meter mapa para guardar la ubicacion del usuario

--- pages/home/user/index.js
+++ pages/home/user/index.js
@@ -61,9 +61,46 @@ const User = () => {
                 <Spacer />
                 
             </HStack>
-        </Box>
-        <Box>
-            
+            <Link href="/home/user/location">
+            <Button colorScheme="teal" variant="outline" size="lg" left={10} top={10}>Cambiar ubicacion</Button>
+            </Link>
+            <Spacer />
+        <TableContainer>
+            <Table variant='striped' colorScheme='teal'>
+                <TableCaption>Imperial to metric conversion factors</TableCaption>
+                <Thead>
+                <Tr>
+                    <Th>To convert</Th>
+                    <Th>into</Th>
+                    <Th isNumeric>multiply by</Th>
+                </Tr>
+                </Thead>
+                <Tbody>
+                <Tr>
+                    <Td>inches</Td>
+                    <Td>millimetres (mm)</Td>
+                    <Td isNumeric>25.4</Td>
+                </Tr>
+                <Tr>
+                    <Td>feet</Td>
+                    <Td>centimetres (cm)</Td>
+                    <Td isNumeric>30.48</Td>
+                </Tr>
+                <Tr>
+                    <Td>yards</Td>
+                    <Td>metres (m)</Td>
+                    <Td isNumeric>0.91444</Td>
+                </Tr>
+                </Tbody>
+                <Tfoot>
+                <Tr>
+                    <Th>To convert</Th>
+                    <Th>into</Th>
+                    <Th isNumeric>multiply by</Th>
+                </Tr>
+                </Tfoot>
+            </Table>
+            </TableContainer>
         </Box>
         </>
 
