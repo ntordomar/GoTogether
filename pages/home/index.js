ff866a1041ccca1258d32ef05abbb72e5e893069
ntordomar <ntordomar@itba.edu.ar>
2023-04-01 06:07:29 -0300

    Pagina de inicio creada

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,30 +1,36 @@
-import { Box,Stack, Input, Button, Center, HStack } from "@chakra-ui/react";
+import {Link, Text, Box,Stack, Heading, Button, Center, HStack, Flex, Grid, GridItem} from "@chakra-ui/react";
+import { Component } from "react";
 
 const Home = () => {
   return (
     <>
-   <Box bg = "blue">
-        
-
-    <Stack>
-        <Center>
-        <Input placeholder='Paz pete' /> 
-        </Center>
-        <Button colorScheme='teal'>Enviar</Button>
-        <Center>
-            <HStack>
-                <Button colorScheme='blue'>Enviar</Button>
-                <Button colorScheme='red'>Enviar</Button>
-                <Button colorScheme='pink'>Enviar</Button>
-            </HStack>
-        </Center>
-        </Stack>
-
-        
-    </Box>
-
-    
+        <Box bg = "whiteAlpha.900" >
+            <Stack>
+            
+                <Flex justifyContent="space-between">
+                    <Heading as='h1' size='4xl' left={10} fontFamily={""} color='green.800'>
+                        GoTogether
+                    </Heading>
+                    <Button colorScheme='green' top={4} right={10}>Help</Button>
+                </Flex>
+                <Grid>
+                    <GridItem h='3' weight="100vh" bg='gray.700' /> 
+                </Grid>
+            </Stack>
+        </Box>
+        <Box bg="green.300" height="100vh">
+            <Center>
+                <Stack>
+                    {<Box m = "20" ></Box>}
+                    <Button width="500px" height="100px" colorScheme='green' fontSize={30}>INCIAR SESIÓN</Button>
+                    {<Box h="100" ></Box>}
+                    <Center><Text fontSize="2xl" color="white">¿Primera vez en GoTogether?</Text></Center>
+                    <Button width="500px" height="100px" colorScheme='green' fontSize={30}>REGISTRARSE</Button>                
+                </Stack>
+            </Center>
+        </Box>  
     </>
+    
   );
 }                                       
 export default Home;
